13519375bb06b065ffe2f32420340b999df99dae
Patrick J <patrick.pddstudio@googlemail.com>
2016-07-06 10:07:18 +0200

    Removed unused code

--- app/src/main/java/com/pddstudio/highlightjs/demo/MainActivity.java
+++ app/src/main/java/com/pddstudio/highlightjs/demo/MainActivity.java
@@ -7,9 +7,6 @@ import com.pddstudio.highlightjs.demo.fragments.FilesListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private SwipeRefreshLayout swipeRefreshLayout;
-    //private HighlightJsView highlightJsView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +15,6 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.activity_main, FilesListFragment.newInstance())
                 .commit();
-        /*createUrls();
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
-        swipeRefreshLayout.setOnRefreshListener(this);
-        highlightJsView = (HighlightJsView) findViewById(R.id.highlight_js_view);
-        highlightJsView.setTheme(Theme.DEFAULT);
-        highlightJsView.setHighlightLanguage(Language.AUTO_DETECT);
-        highlightJsView.setSource(javaUrl);*/
-        //RepositoryLoader.get().loadFiles(null);
-
     }
 
 }
