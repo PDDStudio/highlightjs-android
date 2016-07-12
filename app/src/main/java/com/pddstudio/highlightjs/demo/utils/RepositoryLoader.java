430b861fbc7865074361bad70fcca22db123bb37
Patrick Jung <patrick.jung@arconsis.com>
2016-07-12 15:42:50 +0200

    Added support for more file types to test gainst parsing issues

--- app/src/main/java/com/pddstudio/highlightjs/demo/utils/RepositoryLoader.java
+++ app/src/main/java/com/pddstudio/highlightjs/demo/utils/RepositoryLoader.java
@@ -20,81 +20,92 @@ import java.util.Set;
 
 public class RepositoryLoader {
 
-    public interface Callback {
-        void onItemLoaded(FileObject fileObject);
-        void onFilesLoaded(List<FileObject> fileObjects);
-    }
+	public interface Callback {
+		void onItemLoaded(FileObject fileObject);
 
-    private static RepositoryLoader repositoryLoader;
+		void onFilesLoaded(List<FileObject> fileObjects);
+	}
 
-    private GitHub gitHub;
-    private List<FileObject> fileObjectList = new LinkedList<>();
-    private Callback callback;
+	private static RepositoryLoader repositoryLoader;
 
-    private RepositoryLoader() {
-        try {
-            gitHub = GitHub.connectAnonymously();
-        } catch (IOException io) {
-            io.printStackTrace();
-        }
-    }
+	private GitHub gitHub;
+	private List<FileObject> fileObjectList = new LinkedList<>();
+	private Callback callback;
 
-    public static RepositoryLoader get() {
-        if(repositoryLoader == null) repositoryLoader = new RepositoryLoader();
-        return repositoryLoader;
-    }
+	private RepositoryLoader() {
+		try {
+			gitHub = GitHub.connectAnonymously();
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+	}
 
-    public void loadFiles(Callback callback) {
-        this.callback = callback;
-        new RepoLoader().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-    }
+	public static RepositoryLoader get() {
+		if (repositoryLoader == null) {
+			repositoryLoader = new RepositoryLoader();
+		}
+		return repositoryLoader;
+	}
 
-    private class RepoLoader extends AsyncTask<Void, FileObject, Void> {
+	public void loadFiles(Callback callback) {
+		this.callback = callback;
+		new RepoLoader().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	}
 
-        @Override
-        protected void onPreExecute() {
-            Log.d(getClass().getSimpleName(), "loadFiles() - starting async call");
-        }
+	private class RepoLoader extends AsyncTask<Void, FileObject, Void> {
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Log.d(getClass().getSimpleName(), "entering try-catch block / Anonymous: " + gitHub.isAnonymous());
-                GHRepository ghRepository = gitHub.getRepository("PDDStudio/highlightjs-android");
-                Log.d(getClass().getSimpleName(), "GHURepository null: " + (ghRepository == null));
-                Set<GHCommit> commitSet = ghRepository.listCommits().asSet();
-                Log.d(getClass().getSimpleName(), "GHCommit size: " + commitSet.size());
-                for(GHCommit commit : commitSet) {
-                    List<GHCommit.File> files = commit.getFiles();
-                    for(GHCommit.File file : files) {
-                        fileObjectList.add(new FileObject(file.getFileName(), file.getRawUrl()));
-                        publishProgress(new FileObject(file.getFileName(), file.getRawUrl()));
-                    }
-                }
-                Log.d(getClass().getSimpleName(), "Loaded Items: " + fileObjectList.size());
-            } catch (IOException io) {
-                io.printStackTrace();
-            } finally {
-                Log.d(getClass().getSimpleName(), "Leaving AsyncTask");
-            }
-            return null;
-        }
+		@Override
+		protected void onPreExecute() {
+			Log.d(getClass().getSimpleName(), "loadFiles() - starting async call");
+		}
 
-        @Override
-        protected void onProgressUpdate(FileObject... values) {
-            Log.d(getClass().getSimpleName(), "onProgressUpdate() called.");
-            if(callback != null && isFileType(values[0])) callback.onItemLoaded(values[0]);
-        }
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Log.d(getClass().getSimpleName(), "entering try-catch block / Anonymous: " + gitHub.isAnonymous());
+				GHRepository ghRepository = gitHub.getRepository("PDDStudio/highlightjs-android");
+				Log.d(getClass().getSimpleName(), "GHURepository null: " + (ghRepository == null));
+				Set<GHCommit> commitSet = ghRepository.listCommits().asSet();
+				Log.d(getClass().getSimpleName(), "GHCommit size: " + commitSet.size());
+				for (GHCommit commit : commitSet) {
+					List<GHCommit.File> files = commit.getFiles();
+					for (GHCommit.File file : files) {
+						fileObjectList.add(new FileObject(file.getFileName(), file.getRawUrl()));
+						publishProgress(new FileObject(file.getFileName(), file.getRawUrl()));
+					}
+				}
+				Log.d(getClass().getSimpleName(), "Loaded Items: " + fileObjectList.size());
+			} catch (IOException io) {
+				io.printStackTrace();
+			} finally {
+				Log.d(getClass().getSimpleName(), "Leaving AsyncTask");
+			}
+			return null;
+		}
 
-        private boolean isFileType(FileObject fileObject) {
-            if(fileObject.getName().endsWith(".md") || fileObject.getName().endsWith(".java") || fileObject.getName().endsWith(".gradle")) return true;
-            return false;
-        }
+		@Override
+		protected void onProgressUpdate(FileObject... values) {
+			Log.d(getClass().getSimpleName(), "onProgressUpdate() called.");
+			if (callback != null && isFileType(values[0])) {
+				callback.onItemLoaded(values[0]);
+			}
+		}
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            if(callback != null) callback.onFilesLoaded(fileObjectList);
-        }
-    }
+		private boolean isFileType(FileObject fileObject) {
+			if (fileObject.getName().endsWith(".md") || fileObject.getName().endsWith(".java") || fileObject.getName().endsWith(".gradle") || fileObject.getName()
+																																						.endsWith(".php") || fileObject
+					.getName()
+					.endsWith(".h") || fileObject.getName().endsWith(".xml")) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		protected void onPostExecute(Void aVoid) {
+			if (callback != null)
+				callback.onFilesLoaded(fileObjectList);
+		}
+	}
 
 }
