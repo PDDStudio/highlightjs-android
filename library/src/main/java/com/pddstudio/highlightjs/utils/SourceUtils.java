97845cf9edb62b16623f394eef50ebecf3629dbc
Patrick J <patrick.pddstudio@googlemail.com>
2017-03-23 13:55:29 +0100

    Refactored source utils to reduce code redundancy and support for line numbers

--- library/src/main/java/com/pddstudio/highlightjs/utils/SourceUtils.java
+++ library/src/main/java/com/pddstudio/highlightjs/utils/SourceUtils.java
@@ -11,48 +11,58 @@ import android.support.annotation.Nullable;
 
 public class SourceUtils {
 
-	private static final String SYNTAX_TEMPLATE_HEADER = "<!DOCTYPE html>\n" +
-			"<html>\n" +
-			"<head>\n" +
-			"    <meta charset=\"utf-8\">\n" +
-			"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0\">\n" +
-			"    <style type=\"text/css\">\n" +
-			"       html, body {\n" +
-			"           width:100%;\n" +
-			"           height: 100%;\n" +
-			"           margin: 0px;\n" +
-			"           padding: 0px;\n" +
-			"       }\n" +
-			"   </style>";
+	public static String generateContent(String source, @NonNull String style, @Nullable String language, boolean supportZoom, boolean showLineNumbers) {
+		return getStylePageHeader(supportZoom) +
+				getSourceForStyle(style) +
+				(showLineNumbers ? getLineNumberStyling() : "") +
+				getScriptPageHeader(showLineNumbers) +
+				getSourceForLanguage(source, language) +
+				getTemplateFooter();
+	}
 
-	private static final String SYNTAX_TEMPLATE_HEADER_ZOOMING = "<!DOCTYPE html>\n" +
-			"<html>\n" +
-			"<head>\n" +
-			"    <meta charset=\"utf-8\">\n" +
-			"    <style type=\"text/css\">\n" +
-			"       html, body {\n" +
-			"           width:100%;\n" +
-			"           height: 100%;\n" +
-			"           margin: 0px;\n" +
-			"           padding: 0px;\n" +
-			"       }\n" +
-			"   </style>";
+	private static String getStylePageHeader(boolean enableZoom) {
+		return "<!DOCTYPE html>\n" +
+				"<html>\n" +
+				"<head>\n" +
+				"    <meta charset=\"utf-8\">\n" +
+				(enableZoom ? "" : "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0\">\n") +
+				"    <style type=\"text/css\">\n" +
+				"       html, body {\n" +
+				"           width:100%;\n" +
+				"           height: 100%;\n" +
+				"           margin: 0px;\n" +
+				"           padding: 0px;\n" +
+				"       }\n" +
+				"   </style>\n";
+	}
 
-	private static final String SYNTAX_TEMPLATE_HEADER_2 = "    <script src=\"./highlight.pack.js\"></script>\n" +
-			"    <script>hljs.initHighlightingOnLoad();</script>\n" +
-			"</head>\n" +
-			"<body style=\"margin: 0; padding: 0\">\n";
+	private static String getScriptPageHeader(boolean showLineNumbers) {
+		return "    <script src=\"./highlight.pack.js\"></script>\n" +
+				(showLineNumbers ? "<script src=\"./highlightjs-line-numbers.min.js\"></script>\n" : "") +
+				"    <script>hljs.initHighlightingOnLoad();</script>\n" +
+				(showLineNumbers ? "<script>hljs.initLineNumbersOnLoad();</script>\n" : "") +
+				"</head>\n" +
+				"<body style=\"margin: 0; padding: 0\">\n";
+	}
 
-	private static final String SYNTAX_TEMPLATE_FOOTER = "</body>\n</html>\n";
+	private static String getLineNumberStyling() {
+		return "<style type=\"text/css\">\n" +
+				".hljs-line-numbers {\n" +
+				"\ttext-align: right;\n" +
+				"\tborder-right: 1px solid #ccc;\n" +
+				"\tcolor: #999;\n" +
+				"\t-webkit-touch-callout: none;\n" +
+				"\t-webkit-user-select: none;\n" +
+				"\t-khtml-user-select: none;\n" +
+				"\t-moz-user-select: none;\n" +
+				"\t-ms-user-select: none;\n" +
+				"\tuser-select: none;\n" +
+				"}\n" +
+				"</style>\n";
+	}
 
-	public static String generateContent(String source, @NonNull String style, @Nullable String language, boolean supportZoom) {
-		if (supportZoom) {
-			return SYNTAX_TEMPLATE_HEADER_ZOOMING + getSourceForStyle(style) + SYNTAX_TEMPLATE_HEADER_2 + getSourceForLanguage(source, language) +
-					SYNTAX_TEMPLATE_FOOTER;
-		} else {
-			return SYNTAX_TEMPLATE_HEADER + getSourceForStyle(style) + SYNTAX_TEMPLATE_HEADER_2 + getSourceForLanguage(source,
-																													   language) + SYNTAX_TEMPLATE_FOOTER;
-		}
+	private static String getTemplateFooter() {
+		return "</body>\n</html>\n";
 	}
 
 	private static String formatCode(String code) {
